bc2612bc506eb9ffe7ae7db8c736b0a79642996a
Jason-B0816git config --global user.name Jason-B0816 <jbrooks200800@gmail.com>
2025-01-29 18:35:09 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Generate Read Me 
+This project is to create a ReadMe generator to allow more time dedicated to the project at hand.
+## Installation
+N/A
+## Usage
+Project uses GitHub, VS studios, Node.js, inquirer package
+## Contribution
+Please include all code used, give credit to any packages used
+## Tests
+Test 
+## License
+This project is licensed under the None license.
+## Questions
+If you have any questions, please contact me at [jbrooks200800@gmail.com](mailto:jbrooks200800@gmail.com). You can also find more of my work at [Jason-B0816](https://github.com/Jason-B0816).
---
+++ index.js
@@ -0,0 +1,87 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+const generateReadMe = (answers) =>
+`# ${answers.title}
+${answers.description}
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## Contribution
+${answers.contribution}
+## Tests
+${answers.test}
+## License
+This project is licensed under the ${answers.license} license.
+## Questions
+If you have any questions, please contact me at [${answers.email}](mailto:${answers.email}). You can also find more of my work at [${answers.github}](https://github.com/${answers.github}).
+`;
+// TODO: Create an array of questions for user input
+const questions = [];
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+])
+.then((answers) => {
+    const readMe = generateReadMe(answers);
+    writeToFile('README.md', readMe);
+});
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.log(err) : console.log('Successfully created ReadMe file!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
